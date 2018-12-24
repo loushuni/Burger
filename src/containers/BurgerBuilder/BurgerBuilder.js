36b8a0f15e69dcfeb40aac3650eb7ab23cccd15b
Shuni Lou <loushuni@gmail.com>
2018-12-24 02:05:01 -0800

    added error handlering for fetching data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,13 +26,17 @@ class BurgerBuilder extends Component {
         totalPrice: 4,
         purchaseable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     }
 
     componentDidMount() {
         axios.get('https://my-kitchen-a67ed.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ ingredients: response.data });
+            })
+            .catch(error => {
+                this.setState({ error: true })
             });
     }
 
@@ -119,7 +123,7 @@ class BurgerBuilder extends Component {
         }
 
         let orderSummary = null;
-        let burger = <Spinner />;
+        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
 
         if (this.state.ingredients) {
             burger = (

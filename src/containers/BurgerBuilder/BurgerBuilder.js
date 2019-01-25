63448564abaef9f2f6620a32c9df267d48ae70b7
Shuni Lou <loushuni@gmail.com>
2019-01-24 21:44:01 -0800

    fixed the order now button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,6 @@ class BurgerBuilder extends Component {
     // }
 
     state = {
-        purchaseable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -41,7 +40,8 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({ purchaseable: sum > 0 });
+        //this.setState({ purchaseable: sum > 0 });
+        return sum > 0;
     }
 
     // addIngredientHandler = (type) => {
@@ -116,7 +116,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchaseable={this.state.purchaseable}
+                        purchaseable={this.updatePurchaseState(this.props.ings)}
                         price={this.props.price}
                         ordered={this.purchaseHandler} />
                 </Aux>
